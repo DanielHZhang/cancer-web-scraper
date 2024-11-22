a0474f33391917e8f00327ad5ac46aa70058416d
Daniel Zhang <30360288+DanielHZhang@users.noreply.github.com>
2024-11-21 23:38:46 -0500

    feat: add option to output csv only

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 import { chromium } from "@playwright/test";
 import { Command } from "commander";
-import { type Drug } from "./db";
+import { db, type Drug } from "./db";
 import { batchPages } from "./utils/batch";
 import { writeResultsCsv } from "./utils/csv";
 import { parseIntArg } from "./utils/number";
@@ -14,15 +14,23 @@ import { getFdaInfo } from "./web/fda-gov/api";
 interface Argv {
 	cancer?: string;
 	drugLimit: number;
+	output: boolean;
 }
 
 async function main() {
 	const program = new Command()
 		.option("-c, --cancer <type>", "limit scraping to specific cancer type", (value) => value.toLowerCase())
 		.option("-dl, --drug-limit [number]", "limit number of drugs scraped per cancer type", parseIntArg, -1)
+		.option("-o --output", "outputs the database in csv without scraping", false)
 		.parse();
 	const args = program.opts<Argv>();
 	console.log("Config:", args);
+
+	if (args.output) {
+		console.log("Output CSV only, skipping scraping.");
+		return await outputCsv();
+	}
+
 	const browser = await chromium.launch({ headless: false });
 	const context = await browser.newContext();
 
@@ -58,6 +66,12 @@ async function main() {
 		await context.close();
 		await browser.close();
 	}
+}
+
+async function outputCsv() {
+	const cancers = await db.query.cancers.findMany();
+	const drugs = await db.query.drugs.findMany();
+	await writeResultsCsv(cancers, drugs);
 }
 
 main().catch(console.error);
